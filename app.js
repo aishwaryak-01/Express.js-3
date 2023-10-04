bbdf5365ed2c9f976021b290ce04d3ab359379d1
aishwaryak-01 <133705983+aishwaryak-01@users.noreply.github.com>
2023-10-04 08:30:27 +0530

    app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const express=require('express');
-const bodyParser=require('body-parser');
-const app=express();
-const adminRouter=require('./router/admin');
-const shopRouter=require('./router/shop');
-app.use(bodyParser.urlencoded({extended:false}));
-app.use('/admin',adminRouter);
-app.use('/shop', shopRouter);
-app.use( (request, response, next) => {
-    response.status(404).send('<h1>Page not found</h1>');
+const express = require('express');
+const bodyParser = require('body-parser');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+const app = express();
+app.use(bodyParser.urlencoded({extended: false}));
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+app.use('/', (req, res, next) => {
+    res.status(404).send('<h1>Page not found!</h1>');
 })
 app.listen(3000);
